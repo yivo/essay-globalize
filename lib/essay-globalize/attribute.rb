6a634aa77be98257a7de3557c9931d22b3a9493e
Yaroslav Konoplov <yaroslav@inbox.com>
2016-06-07 20:24:08 +0300

    ruby 2.3 frozen string literals

--- lib/essay-globalize/attribute.rb
+++ lib/essay-globalize/attribute.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class Essay::AttributeFeatures
   def translates?
     translates_with_globalize?
--- lib/essay-globalize/model.rb
+++ lib/essay-globalize/model.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class Essay::ModelFeatures
   def translates?
     translates_with_globalize?
